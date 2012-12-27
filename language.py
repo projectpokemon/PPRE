c251b0588e6ae739964a242a62ea6431167fd476
Alpha <alpha@pokesplash.net>
2012-12-27 01:12:28 -0500

    Better translations

--- language.py
+++ language.py
@@ -1,1 +1,18 @@
-from languages.en_US import translations 
+from languages.en_US import translations
+defaulttranslations = translations
+
+def setLanguage(lang):
+    global translations
+    newlang = __import__(lang)
+    translations = newlang.translations
+    
+def unsetLanguage():
+    global translations
+    translations = defaulttranslations
+
+def translate(phrase):
+    if phrase in translations:
+        return translations[phrase]
+    if phrase in defaulttranslations:
+        return defaulttranslations[phrase]
+    return phrase
