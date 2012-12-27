703281f9d5c59c1de3ef4af265d3dd0ff6024723
Alpha <alpha@pokesplash.net>
2012-12-27 00:51:30 -0500

    Using modular field generation

--- genpokedex.py
+++ genpokedex.py
@@ -1,34 +1,21 @@
 from gen import *
-from nds import narc
+from nds import narc, fieldgen
 import struct
 
 FNAME = "pokedex"+FEXT
 
 for game in games:
-    fmt = dexfmt[game].pop(0)
-    ofile = template.open(STATIC_DIR+game+"/"+FORMAT_SUBDIR+FNAME, "w", "Pokemon %s Pokedex Format"%game.title())
-    ofile.write("""
-<h2>Pokemon %s Pokedex/Personal Format</h2>
-<p>Structure Size: %d bytes</p>\n<table>\n"""%(game.title(), struct.calcsize(fmt)))
-    ofs = 0
-    for i, entry in enumerate(dexfmt[game]):
-        ofile.write("<tr><td>%d</td><td>%d</td><td>%s</td></tr>"%(ofs, struct.calcsize(fmt[i]), entry[0]))
-        ofs += struct.calcsize(fmt[i])
-    ofile.write("</table>\n")
+    title = "Pokemon %s Pokedex/Personal Format"%game.title()
+    ofile = template.open(STATIC_DIR+game+"/"+FORMAT_SUBDIR+FNAME, "w", title)
+    fieldgen.generateFormatHTML(dexfmt[game], title, ofile)
     ofile.close()
-    ofile = template.open(STATIC_DIR+game+"/"+FNAME, "w", "Pokemon %s Pokedex Data"%game.title())
+    ofile = template.open(STATIC_DIR+game+"/"+FNAME, "w", title)
     ofile.write("""
 <h2>Pokemon %s Pokemon Data</h2>
 <h3>%s - NARC Container</h3>
 <p><a href='./%s%s'>Format</a></p>
-<table>\n"""%(game.title(), POKEDEX_FILE[game], FORMAT_SUBDIR, FNAME))
-    n = narc.NARC(open(DATA_DIR+game+"/fs/"+POKEDEX_FILE[game], "rb").read())
-    for j, f in enumerate(n.gmif.files):
-        ofile.write("\t<tr><td><h4>Pokemon Id #%d</h4></td></tr>\n"%j)
-        dexdata = struct.unpack(fmt, f[:struct.calcsize(fmt)])
-        for i, entry in enumerate(dexfmt[game]):
-            if entry[0] == "pad" and dexdata[i]:
-                print(j, dexdata[i])
-            ofile.write("\t<tr><td>%s</td><td>%d</td></tr>\n"%(entry[0], dexdata[i]))
-    ofile.write("</table>")
+"""%(game.title(), POKEDEX_FILE[game], FORMAT_SUBDIR, FNAME))
+    fieldgen.makeHtmlEntries(dexfmt[game], 
+        DATA_DIR+game+"/fs/"+POKEDEX_FILE[game], ofile)
     ofile.close()
+exit()
