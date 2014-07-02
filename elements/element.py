d067c299c06788c40b63b69fb6af877fbed2739a
Alpha <alpha@pokesplash.net>
2014-07-01 23:08:19 -0400

    Element.save()

--- elements/element.py
+++ elements/element.py
@@ -2,10 +2,18 @@
 
 class GameAdapter(object):
     def __init__(self, game):
+        self.observed = {}
         self.game = game
 
     def load_archive(self, filename):
+        """ Load archive from local filename """
         self.archive = self.game.archive(filename)
+
+    def save(self):
+        """ Save changes to archive """
+        # TODO: Pass observed dict to archive.
+        if self.observed:
+            self.archive.write()
 
 
 class BaseElement(GameAdapter):
@@ -13,6 +21,7 @@ class BaseElement(GameAdapter):
         return len(self.archive)
 
     def __getitem__(self, key):
+        self.observed[key] = True
         return self.atom(self.archive[key])
 
     def __iter__(self):
