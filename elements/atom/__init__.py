aab66077ada50d2a80e5e2b4551afbb666dca703
Alpha <alpha@pokesplash.net>
2014-07-23 23:00:39 -0400

    Made atomic package available.

--- elements/atom/__init__.py
+++ elements/atom/__init__.py
@@ -1,5 +1,27 @@
+"""
 
-from atomic import Atomic
+>>> from rawdb.elements.atom import BaseAtom
+>>> class MyAtom(BaseAtom):
+...     def __init__(self):
+...         super(MyAtom, self).__init__()
+...         self.int8('field1')
+...         self.uint8('field2')
+...         self.uint16('field3')
+...
+>>> atom = MyAtom()
+>>> entry = atom('\xff\xff\x02\x01')
+>>> entry.field1
+-1
+>>> entry.field3
+258
+>>> entry.field3 = 100
+>>> str(entry)
+'\xff\xff\x64\x00'
+
+"""
+
+from atomic import AtomicInstance
 from base_atom import BaseAtom
+from valence import ValenceFormatter
 
-__all__ = ['Atomic', 'BaseAtom']
+__all__ = ['AtomicInstance', 'BaseAtom', 'ValenceFormatter']
