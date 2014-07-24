b5f10d42795c268c880d87616603020b5ee0f53a
Alpha <alpha@pokesplash.net>
2014-07-23 22:52:55 -0400

    Moved AtomicInstance to own module (partial commit)

---
+++ elements/atom/atomic.py
@@ -0,0 +1,47 @@
+
+class AtomicInstance(object):
+    """
+
+    Methods
+    -------
+    keys : list
+        Get the valent attributes/keys of this atom
+    """
+    def __init__(self, atom, consumer):
+        super(AtomicInstance, self).__setattr__('_frozen', False)
+        super(AtomicInstance, self).__setattr__('_attrs', {})
+        super(AtomicInstance, self).__setattr__('_atom', atom)
+        super(AtomicInstance, self).__setattr__('_data', consumer)
+
+    def keys(self):
+        return self._attrs.keys()
+
+    @property
+    def data(self):
+        return self._data
+
+    def freeze(self):
+        super(AtomicInstance, self).__setattr__('_frozen', True)
+
+    def __getattr__(self, name):
+        return self._attrs[name]
+
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+
+    def __setattr__(self, name, value):
+        if self._frozen and name not in self._attrs:
+            raise KeyError(name)
+        self._attrs[name] = value
+
+    def __setitem__(self, key, value):
+        return self.__setattr__(key, value)
+
+    def __str__(self):
+        return self._atom.pack(self._attrs)
+
+    def __dict__(self):
+        return self._attrs
+
+    def __dir__(self):
+        return self.keys()
