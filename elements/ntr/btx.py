e9c44fd3121b3f54b78969aeddcbc28de626ffca
Alpha <alpha@pokesplash.net>
2014-08-03 18:11:44 -0400

    Much more reasonable BTX approximation

--- elements/ntr/btx.py
+++ elements/ntr/btx.py
@@ -3,48 +3,33 @@
 from rawdb.elements.atom import BaseAtom, ValenceFormatter
 
 
-class SeekValence(ValenceFormatter):
-    def __init__(self, field):
-        super(SeekValence, self).__init__(None)
-        self.field = field
-        self.ignore = True
-        self.unpack_one = self.unpack_one_seek
-
-    def unpack_one_seek(self, atomic):
-        if hasattr(self.field, '__call__'):
-            atomic.data.seek(self.field(atomic), 0)
-        else:
-            atomic.data.seek(atomic[self.field], 0)
-        return None
-
-
 class BTXAtom(BaseAtom):
     def __init__(self):
         super(BTXAtom, self).__init__()
-        self.uint32('magic')
+        start = self.uint32('magic')
         self.uint32('size')
-        self.texinfo('texinfo')
-        self.texinfo('tex4x4info')
+        texinfo = self.texinfo('texinfo')
+        tex4x4info = self.texinfo('tex4x4info')
         self.uint32('tex4x4info_paletteofs')
-        self.texinfo('palinfo', True)
-        self.append_format(SeekValence(lambda atomic:
-                                       atomic.texinfo.lookupofs))
+        palinfo = self.texinfo('palinfo', True)
+
+        self.seek(texinfo.lookupofs, start=start)
         self.lookupdict('texdict')
-        self.append_format(SeekValence(lambda atomic:
-                                       atomic.palinfo.lookupofs))
+
+        self.seek(palinfo.lookupofs, start=start)
         self.lookupdict('paldict')
 
-        self.append_format(SeekValence(lambda atomic:
-                                       atomic.texinfo.dataofs))
-        self.data('texdata', lambda atomic: atomic.texinfo.datasize << 3)
+        self.seek(texinfo.dataofs, start=start)
+        # self.data('texdata', texinfo.datasize << 3)
+        self.data('texdata', texinfo.datasize)
 
-        self.append_format(SeekValence(lambda atomic:
-                                       atomic.tex4x4info.dataofs))
-        self.data('tex4x4data', lambda atomic: atomic.tex4x4info.datasize << 3)
+        self.seek(tex4x4info.dataofs, start=start)
+        # self.data('tex4x4data', tex4x4info.datasize << 3)
+        self.data('tex4x4data', tex4x4info.datasize)
 
-        self.append_format(SeekValence(lambda atomic:
-                                       atomic.palinfo.dataofs))
-        self.data('paldata', lambda atomic: atomic.palinfo.datasize << 3)
+        self.seek(palinfo.dataofs, start=start)
+        # self.data('paldata', palinfo.datasize << 3)
+        self.data('paldata', palinfo.datasize)
 
     def texinfo(self, name, late_lookup=False):
         """
@@ -66,11 +51,11 @@ class BTXAtom(BaseAtom):
             self.uint16('lookupofs')
         self.uint16('dummy0')
         self.uint32('dataofs')
-        self.sub_pop()
+        return self.sub_pop()
 
     def lookupdict(self, name):
         self.sub_push(name)
-        self.uint8('version')
+        start = self.uint8('version')
         num = self.uint8('num')
         self.uint16('size')
         self.uint16('dummy0')
@@ -83,13 +68,12 @@ class BTXAtom(BaseAtom):
         self.uint8('index')
         self.array(self.sub_pop(), count=num)
 
-        # self.seek(lambda atomic: atomic.refofs)
-        # self.append_format(SeekValence('refofs'))
+        self.seek(refofs, start=start)
         sizeunit = self.uint16('sizeunit')
-        self.uint16('nameofs')
-        self.array(self.uint8('data'), count=num*sizeunit)
-        # self.append_format(SeekValence('nameofs'))
+        nameofs = self.uint16('nameofs')
+        # self.array(self.uint8('data'), count=num*sizeunit)
+        self.array(self.uint8('data'), count=num)
+        self.seek(nameofs, start=start)
         self.array(self.string('names', 16), count=num)
 
-        lookup = self.sub_pop()
-        self.offset(refofs, start=lookup, dest=sizeunit)
+        return self.sub_pop()
