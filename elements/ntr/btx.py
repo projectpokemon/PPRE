519f6b5e3c6ad352836cf4b52f798b73a6813f1c
Alpha <alpha@pokesplash.net>
2014-08-04 19:03:23 -0400

    BTX Atomic

--- elements/ntr/btx.py
+++ elements/ntr/btx.py
@@ -1,9 +1,38 @@
 """Texture Atoms"""
 
-from rawdb.elements.atom import BaseAtom, ValenceFormatter
+import struct
+
+from rawdb.elements.atom import BaseAtom, AtomicInstance
+
+
+class BTXAtomicInstance(AtomicInstance):
+    @property
+    def images(self):
+        images = []
+        for param in self.texparams:
+            images.append(0)
+        return images
+
+    @property
+    def texparams(self):
+        # 8 bytes per param
+        params = []
+        for i in xrange(self.texdict.num):
+            imgParam, extra = struct.unpack('II',
+                                            self.texdict.data_[i*8:i*8+8])
+            params.append({
+                'ofs': (imgParam & 0xFFFF) << 3,
+                'width': (imgParam >> 20) & 0x7,
+                'height': (imgParam >> 23) & 0x7,
+                'format': (imgParam >> 26) & 0x7,
+                'enable': (imgParam >> 29) & 0x1
+            })
+        return params
 
 
 class BTXAtom(BaseAtom):
+    atomic = BTXAtomicInstance
+
     def __init__(self):
         super(BTXAtom, self).__init__()
         start = self.uint32('magic')
@@ -68,7 +97,7 @@ class BTXAtom(BaseAtom):
         self.seek(refofs, start=start)
         sizeunit = self.uint16('sizeunit')
         nameofs = self.uint16('nameofs')
-        data = self.array(self.uint8('data'), count=num*sizeunit)
+        self.data('data_', num*sizeunit)
         self.seek(nameofs, start=sizeunit)
         self.array(self.string('names', 16), count=num)
 
