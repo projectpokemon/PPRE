6c829b30f102f0cd061f44a603f077a54399ffd7
Alpha <alpha@pokesplash.net>
2014-09-21 18:32:05 -0400

    Base Archive class

---
+++ generic/archive.py
@@ -0,0 +1,25 @@
+
+import abc
+
+
+class Archive(object):
+    __metaclass__ = abc.ABCMeta
+    files = abc.abstractproperty()
+
+    def get(self, ref):
+        return self.files[ref]
+
+    def add(self, ref, data):
+        self.files[ref] = data
+
+    def delete(self, ref):
+        self.files.pop(ref)
+
+    def __iter__(self):
+        return self.files
+
+    def __len__(self):
+        return len(self.files)
+
+    def save(self):
+        pass
