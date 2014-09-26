472dd72bee55743c33a6e7a3f426975d1a9b7b44
Alpha <alpha@pokesplash.net>
2014-09-26 14:58:09 -0400

    TEX Saving

--- ntr/g3d/btx.py
+++ ntr/g3d/btx.py
@@ -6,6 +6,11 @@ from rawdb.ntr.g3d.resdict import G3DResDict
 from rawdb.util.io import BinaryIO
 
 
+def log2(x):
+    """Integer log2"""
+    return x.bit_length()-1
+
+
 class TexInfo(object):
     INFO_TEX = 0
     INFO_TEX4X4 = 1
@@ -44,14 +49,17 @@ class TexInfo(object):
         writer.writeUInt16(0)  # datasize
         if self.infotype != TexInfo.INFO_PAL:
             self._lookupofs_ofs = writer.tell()
+            writer.writeUInt16(0)
         writer.writeUInt16(0)
         if self.infotype == TexInfo.INFO_PAL:
             self._lookupofs_ofs = writer.tell()
+            writer.writeUInt16(0)
         writer.writeUInt16(8)
         self._dataofs_ofs = writer.tell()
         writer.writeUInt32(0)
         if self.infotype == TexInfo.INFO_TEX4X4:
             writer.writeUInt32(0)
+        return writer
 
 
 TexParam = namedtuple('TexParam', 'ofs width height format color0')
@@ -103,3 +111,59 @@ class TEX(Archive):
         # TODO 4x4
         if size:
             reader.seek(start+size)
+
+    def save(self, writer=None):
+        writer = writer if writer is not None else BinaryIO()
+        start = writer.tell()
+        writer.write(self.magic)
+        size_ofs = writer.tell()
+        writer.writeUInt32(0)
+        writer = self.texinfo.save(writer)
+        writer = self.tex4x4info.save(writer)
+        writer = self.palinfo.save(writer)
+
+        writer.writeAlign(4)
+        ofs = writer.tell()-start
+        with writer.seek(self.texinfo._lookupofs_ofs):
+            writer.writeUInt16(ofs)
+        writer = self.texdict.save(writer)
+
+        writer.writeAlign(4)
+        ofs = writer.tell()-start
+        with writer.seek(self.palinfo._lookupofs_ofs):
+            writer.writeUInt16(ofs)
+        writer = self.paldict.save(writer)
+
+        writer.writeAlign(8)
+        ofs = writer.tell()-start
+        with writer.seek(self.texinfo._dataofs_ofs):
+            writer.writeUInt32(ofs)  # texinfo dataofs
+        datastart = writer.tell()
+        for i in xrange(self.texdict.num):
+            texparam = self.texparams[i]
+            writer.writeUInt32((texparam.ofs >> 3) |
+                               ((log2(texparam.width) >> 3) << 20) |
+                               ((log2(texparam.height) >> 3) << 23) |
+                               (texparam.format << 26) |
+                               (texparam.color0 << 29))
+            writer.writeUInt32(0)
+        writer.writeAlign(8)
+        size = writer.tell()-datastart
+        with writer.seek(self.texinfo._datasize_ofs):
+            writer.writeUInt16(size >> 3)  # texinfo datasize
+
+        writer.writeAlign(8)
+        ofs = writer.tell()-start
+        with writer.seek(self.palinfo._dataofs_ofs):
+            writer.writeUInt32(ofs)  # palinfo dataofs
+        datastart = writer.tell()
+        for i in xrange(self.texdict.num):
+            param = self.palparams[i]
+            writer.writeUInt16(param.ofs)
+            writer.writeUInt16(param.count4)
+        writer.writeAlign(8)
+        size = writer.tell()-datastart
+        with writer.seek(self.palinfo._datasize_ofs):
+            writer.writeUInt16(size >> 3)  # palinfo datasize
+
+        return writer
