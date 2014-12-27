8e9658224363a08b87f6c1e7a061706bc4e100c6
Alpha <alpha@pokesplash.net>
2014-12-27 17:05:13 -0500

    Atomic API Updated using proper c_ names, reference to base struct via base_struct, embed as an anonymous embed

--- atomic/atomic_struct.py
+++ atomic/atomic_struct.py
@@ -15,34 +15,44 @@ class AtomicStruct(object):
     def __init__(self, name=None):
         self._fields = []
         self._field_pos = 0
-        self.type = None
-        self.data = None
+        self._pack = 32
+        self._anonymous = []
+        self._type = None
+        self._data = None
         if name is None:
             self.name = self.__class__.__name__
 
     def _add(self, name, type_):
-        if self.data is not None:
+        if self._data is not None:
             raise AtomicError('Frozen Atoms cannot be modified')
         self._fields.insert(self._field_pos, (name, type_))
         self._field_pos += 1
         return (name, type_)
 
     def uint16(self, name):
-        return self._add(ctypes.uint16(name))
+        return self._add(name, ctypes.c_uint16)
 
-    def embed(self, name, type_callable):
+    def embed(self, name, type_callable, base_obj=None):
+        base_obj = base_obj or self
+        self._anonymous.append(name)
         return self._add(name, type_callable())
 
-    def array(self, name, type_callable, length=1):
+    def array(self, name, type_callable, base_obj=None, length=1):
+        base_obj = base_obj or self
         return self._add(name, type_callable() * length)
 
-    def struct(self):
-        return self.type
+    def struct(self, name, type_callable, base_obj=None):
+        base_obj = base_obj or self
+        return self._add(name, type_callable())
+
+    def base_struct(self, name):
+        return (name, self._type)
 
     def __call__(self):
-        return self.type
+        return self._type
 
     def freeze(self):
-        self.type = type(self.name, (ctypes.Structure),
-                         dict(_fields_=self._fields))
-        self.data = self()
+        self._type = type(self.name, (ctypes.Structure),
+                          dict(_fields_=self._fields, _pack_=self._pack,
+                               _anonymous_=tuple(self._anonymous)))
+        self._data = self()
