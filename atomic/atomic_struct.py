b4a4e87fc95cc458446b38eda6d99c77049ba01b
Alpha <alpha@pokesplash.net>
2014-12-27 19:04:11 -0500

    Provided width and default keywords for Atomic types

--- atomic/atomic_struct.py
+++ atomic/atomic_struct.py
@@ -25,12 +25,14 @@ class AtomicContext(object):
 
 
 class AtomicStruct(object):
+    alignment = 32
+
     def __init__(self, name=None):
         self._fields = []
-        self._pack = 32
         self._anonymous = []
         self._type = None
         self._data = None
+        self._defaults = {}
         self.context = {
             'field_pos': 0,
             'simulate': False
@@ -38,16 +40,28 @@ class AtomicStruct(object):
         if name is None:
             self.name = self.__class__.__name__
 
-    def _add(self, name, type_):
+    def _add(self, name, type_, **kwargs):
         if not self.context['simulate']:
             if self._data is not None:
                 raise AtomicError('Frozen Atoms cannot be modified')
-            self._fields.insert(self.context['field_pos'], (name, type_))
+            if 'width' in kwargs:
+                field = (name, type_, kwargs['width'])
+            else:
+                field = (name, type_)
+            self._fields.insert(self.context['field_pos'], field)
             self.context['field_pos'] += 1
+            if 'default' in kwargs:
+                self._defaults[name] = kwargs['default']
         return (name, type_)
 
     def simulate(self):
         return AtomicContext(self, 'simulate')
+
+    def uint8(self, name, **kwargs):
+        return self._add(name, ctypes.c_uint8, **kwargs)
+
+    def int8(self, name, **kwargs):
+        return self._add(name, ctypes.c_int8, **kwargs)
 
     def uint16(self, name):
         return self._add(name, ctypes.c_uint16)
@@ -73,8 +87,13 @@ class AtomicStruct(object):
     def __call__(self):
         return self._type
 
+    def __len__(self):
+        return ctypes.sizeof(self._data)
+
     def freeze(self):
-        self._type = type(self.name, (ctypes.Structure, ),
-                          dict(_fields_=self._fields, _pack_=self._pack,
+        self._type = type('struct_'+self.name, (ctypes.Structure, ),
+                          dict(_fields_=self._fields, _pack_=self.alignment,
                                _anonymous_=tuple(self._anonymous)))
         self._data = self()
+        for key, value in self._defaults.iteritems():
+            setattr(self._data, key, value)
