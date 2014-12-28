b3490bdeabbacf2c92a89e8eea65c1c09b83ca34
Alpha <alpha@pokesplash.net>
2014-12-27 22:40:59 -0500

    Atomic Documentation

--- atomic/atomic_struct.py
+++ atomic/atomic_struct.py
@@ -36,7 +36,19 @@ class AtomicContext(object):
 
 
 class AtomicStruct(object):
-    alignment = 32
+    """Structure and data representation for a given inherited model.
+
+    To use, subclass this. When initializing, call the field building
+    functions (self.uint8('field_a'), etc.) and then freeze it with
+    self.freeze().
+
+    Once the model is frozen, this model has accessible data attributes
+    that can be read or written.
+
+    Use .load() to fill the data from a reader or .save() to build a
+    writer.
+    """
+    alignment = 32  # : Bit alignment
 
     def __init__(self, name=None):
         self._fields = []
@@ -49,9 +61,30 @@ class AtomicStruct(object):
             'simulate': False
         }
         if name is None:
-            self.name = self.__class__.__name__
+            self.name = self.__class__.__name__  # : Name of the type
 
     def _add(self, name, type_, **kwargs):
+        """Adds a field to the structure
+
+        Parameters
+        ----------
+        name : string
+            Identifier. This name should be unique across the struct.
+        type_ : _CData type
+        width : int, optional
+            Number of bits wide
+        default : mixed
+            Default to be filled in when data is cleared/initialized.
+
+        Returns
+        -------
+        field : (str, type)
+
+        Raises
+        ------
+        AtomicError
+            If something cannot be added.
+        """
         if not self.context['simulate']:
             if self._data is not None:
                 raise AtomicError('Frozen Atoms cannot be modified')
@@ -69,16 +102,52 @@ class AtomicStruct(object):
         return (name, type_)
 
     def simulate(self):
+        """Provides a context that does not modify the current fields
+
+        This is automatically used when pulling types out for arrays, et al.
+
+        Returns
+        -------
+        AtomicContext : context
+        """
         return AtomicContext(self, 'simulate')
 
     def _find(self, name):
-        """Get the field position of a field by name"""
+        """Get the field position of a field by name
+
+        Parameters
+        ----------
+        name : str
+            Identifier to lookup
+
+        Returns
+        -------
+        pos : int
+            Index of field
+
+        Raises
+        ------
+        IndexError
+            If the field cannot be looked up by that name"""
         for idx, field in enumerate(self._fields):
             if field[0] == name:
                 return idx
         raise IndexError('Could not find {0}'.format(name))
 
     def after(self, name=None):
+        """Provides a context where modification happens directly after
+        the specified field (by name)
+
+        Parameters
+        ----------
+        name : str, optional
+            Identifier to use as a reference. If not set, the context will
+            refer to the start of the struct instead.
+
+        Returns
+        -------
+        AtomicContext : context
+        """
         if name is None:
             pos = 0
         else:
@@ -86,6 +155,25 @@ class AtomicStruct(object):
         return AtomicContext(self, 'field_pos', pos, rel=True)
 
     def remove(self, name):
+        """Removes a field by name
+
+        Parameters
+        ----------
+        name : str
+            Identifier to lookup and remove
+
+        Returns
+        -------
+        field : (str, type)
+            The removed field
+
+        Raises
+        ------
+        IndexError
+            If the field cannot be found
+        AtomicError
+            If this field could not be removed
+        """
         pos = self._find(name)
         if self.context['simulate']:
             return self._fields[pos]
@@ -122,30 +210,124 @@ class AtomicStruct(object):
         return self.array(name, self.char, self, length=length)
 
     def get_type(self, type_callable, base_obj=None):
+        """Gets the type of a type_callable owned by base_obj
+
+        Parameters
+        ----------
+        type_callable : method
+            Invoked method to generate the type. This expects a
+            field (str, type) to be returned.
+        base_obj : AtomicStruct, optional
+            Owner of the method. This is used to ensure that type_callable is
+            properly simulated. If not specified, the current instance will
+            be used.
+
+        Returns
+        -------
+        type : type
+        """
         with (base_obj or self).simulate():
             return type_callable('_')[1]
 
     def embed(self, name, type_callable, base_obj=None):
+        """Embeds a type into the struct. The type's fields will be accessible
+        without needing to reference `name`.
+
+        Parameters
+        ----------
+        name : str
+        type_callable : method
+        base_obj : AtomicStruct, optional
+
+        See Also
+        --------
+        AtomicStruct.get_type
+
+        Returns
+        -------
+        field : (str, type)
+        """
         self._anonymous.append(name)
         return self._add(name, self.get_type(type_callable, base_obj))
 
     def array(self, name, type_callable, base_obj=None, length=1):
+        """Builds an `length`-long array of a type named `name`
+
+        Parameters
+        ----------
+        name : str
+        type_callable : method
+        base_obj : AtomicStruct, optional
+        length : int
+            Fixed length of the array
+
+        See Also
+        --------
+        AtomicStruct.get_type
+
+        Returns
+        -------
+        field : (str, type)
+        """
         return self._add(name,
                          self.get_type(type_callable, base_obj) * length)
 
     def struct(self, name, type_callable, base_obj=None):
+        """Adds a custom type field directly.
+
+        Use AtomicStruct.base_struct as type_callable for an existing object.
+
+        Parameters
+        ----------
+        name : str
+        type_callable : method
+        base_obj : AtomicStruct, optional
+
+        See Also
+        --------
+        AtomicStruct.get_type
+
+        Returns
+        -------
+        field : (str, type)
+        """
         return self._add(name, self.get_type(type_callable, base_obj))
 
     def base_struct(self, name):
+        """Get a field that refers to this struct as its own type.
+
+        `name` generally is not required, but it is used to match the
+        existing interface.
+
+        Returns
+        -------
+        field : (str, type)
+        """
         return (name, self._type)
 
     def __call__(self):
         return self._type
 
     def __len__(self):
+        """Size of the data structure
+
+        Returns
+        -------
+        size : int
+
+        Raises
+        ------
+        TypeError
+            If this model is not yet frozen.
+        """
         return ctypes.sizeof(self._data)
 
     def freeze(self):
+        """Lock the model's fields in place and compile the custom type.
+
+        No modifications are able to be done after this. This is required
+        before the compiled type and data become accessible.
+        """
         self._type = type('struct_'+self.name, (ctypes.Structure, ),
                           dict(_fields_=self._fields, _pack_=self.alignment,
                                _anonymous_=tuple(self._anonymous)))
@@ -154,12 +336,31 @@ class AtomicStruct(object):
             setattr(self._data, key, value)
 
     def load(self, reader):
+        """Loads a reader into this model
+
+        Parameters
+        ----------
+        reader : BinaryIO, string, file, other readable
+        """
         reader = BinaryIO.reader(reader)
         amount = len(self)
         data = reader.read(amount)
         self._data.from_buffer_copy(data)
 
     def save(self, writer=None):
+        """Creates a writer for this model
+
+        Parameters
+        ----------
+        writer : BinaryIO, optional
+            Destination to write into. If not specified, a new IO will be
+            created.
+
+        Returns
+        -------
+        writer : BinaryIO
+            A new or modified writer that has this data in it
+        """
         writer = writer if writer is not None else BinaryIO()
         amount = len(self)
         writer.write(ctypes.string_at(ctypes.addressof(self._data),
