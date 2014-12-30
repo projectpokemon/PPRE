b51183dee326f7b252e584ac507ea4dc7c867635
Alpha <alpha@pokesplash.net>
2014-12-29 23:35:47 -0500

    new LevelMoves

--- pokemon/poketool/wotbl.py
+++ pokemon/poketool/wotbl.py
@@ -53,3 +53,63 @@ class LevelMoves(Editable):
                 writer.writeUInt16(lvlmove.moveid | (lvlmove.level << 9))
         writer.writeUInt16(0xFFFF)
         return writer
+
+
+from generic.editable import XEditable as Editable
+
+
+class LevelMove(Editable):
+    def __init__(self, version='Diamond'):
+        self.version = version
+        Editable.__init__(self)
+        if version in game.GEN_IV:
+            self.uint16('moveid', width=9, max_value=0x1FF)
+            self.uint16('level', width=7, max_value=0x7F)
+        else:
+            self.uint16('moveid')
+            self.uint16('level')
+        self.freeze()
+
+    def is_end(self):
+        if self.version in game.GEN_IV:
+            return self.moveid == 0x1FF
+        return self.moveid == 0xFFFF
+
+    @staticmethod
+    def end(version='Diamond'):
+        lvlmove = LevelMove(version)
+        if version in game.GEN_IV:
+            lvlmove.moveid = 0x1FF
+            lvlmove.level = 0x7F
+        else:
+            lvlmove.moveid = 0xFFFF
+            lvlmove.level = 0xFFFF
+        return lvlmove
+
+
+class LevelMoves(Editable):
+    def __init__(self, version='Diamond', reader=None):
+        self.version = version
+        Editable.__init__(self)
+        self.array('moves', LevelMove().base_struct, length=0)
+        self.freeze()
+        if reader is not None:
+            self.load(reader)
+
+    def load(self, reader):
+        reader = BinaryIO.reader(reader)
+        self.moves = []
+        while True:
+            lvlmove = LevelMove(self.version)
+            lvlmove.load(reader)
+            if lvlmove.is_end():
+                break
+            else:
+                self.moves.append(lvlmove)
+
+    def save(self, writer=None):
+        writer = writer if writer is not None else BinaryIO()
+        for lvlmove in self.moves:
+            writer = lvlmove.save(writer)
+        writer = LevelMove.end(self.version).save(writer)
+        return writer
