cc0146dd4dcc5b7d891ad3868e2ef10f97f82588
Alpha <alpha@pokesplash.net>
2015-02-21 15:47:42 -0500

    Some of setflag and clearflag

--- pokemon/field/script.c
+++ pokemon/field/script.c
@@ -1,6 +1,8 @@
 
 // Platinum offsets
 
+typedef int (*func_t)(int, int, int, int);
+
 struct script_state {
     unsigned char u0;
     // 0x1
@@ -121,7 +123,7 @@ int func_394b8(int r0, int r1){
     int r5 = r0;
     r0 = ((int*)r5)[12];
     int r4 = r1;
-    func_462ac(r0);
+    func_462ac(r0, r1);
     r1 = 1<<14;
     if(r4 < r1){
         r1 <<= 1;
@@ -145,6 +147,20 @@ int cmd_0003(script_state *r0) {
     return 1;
 }
 
+int func_399e9(script_state *r0) {
+    int r1 = r0->vals[0];
+    r1 &= 0xFFFF;
+    int r0 = r0->u5;
+    r0 = func_394b8(r0, r1);
+    r1 = r0;
+    ((uint16_t *)r0)[0] = r0-1;
+    if(r0 != 1){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
 int cmd_0004(script_state *r0) {
     int r3 = *((unsigned char*)(r0->buf_ptr++));
     int r2 = *((unsigned char*)(r0->buf_ptr++));
@@ -153,4 +169,80 @@ int cmd_0004(script_state *r0) {
     return 0;
 }
 
+int func_462e4(int r0, int r1){
+    int r4 = r1;
+    r0 = func_46338(r0, r1);
+    if(r0 == 0){
+        return 0;
+    }
+    int r5 = r4 >> 31;
+    int r3 = r4 << 29;
+    r3 = r3-r5;
+    int r2 = 29;
+}
+
+int func_462ac(int r0, int r1){
+    func_t r3 = 0x02022611;
+    r1 = 4;
+    // r0 = r3(r0, 4);
+    // 0x02022611
+    int r4 = r1;
+    int r5 = r0;
+    if(r4 < 0x24){
+        r0 = 0x85 << 2;
+        int r2 = r5+r0;
+        r0 = r4 << 4;
+        r1 = r5+r0; // r1 = arg0+(arg1 << 4);
+        r0 = 0x0002022c;
+        r0 = *(r1+r0);
+        return r2 + r0;
+    }else{
+        // 0x20c2c
+        r0 = func_31810(r0);
+        if(r0 == 0){
+            return 0;
+        }
+        r0 = func_cd374(r0);
+        if(r0 == 18){
+            return 18;
+        }
+        return func_8a9b8(r0);
+    }
+}
+
+int func_3953c(int r0, int r1){
+    int r5 = r0;
+    r0 = ((int*)r5)[12];
+    int r4 = r1;
+    r0 = func_462ac(r0, r1);
+    r1 = r4;
+    r0 = func_462e4(r0, r1);
+    return r0;
+}
+
+int func_39550(int r0, int r1){
+    int r5 = r0;
+    r0 = ((int*)r5)[12];
+    int r4 = r1;
+    r0 = func_462ac(r0, r1);
+    r1 = r4;
+    r0 = func_4630c(r0, r1);
+    return r0;
+}
+
+int cmd_0030(script_state *r0){
+    // 0x39e39 - Setflag($1)
+    int r4 = r0->u5;
+    int r1 = read16(r0);
+    int r0 = func_3953c(r4, r1);
+    return 0;
+}
+
+int cmd_0031(script_state *r0){
+    // 0x39e50 - Clearflag($1)
+    int r4 = r0->u5;
+    int r1 = read16(r0);
+    int r0 = func_39550(r4, r1);
+    return 0;
+}
 
