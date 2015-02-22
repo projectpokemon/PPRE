e8865f6a26b72e75fd4a0c83ff69d4f68eb03bd2
Alpha <alpha@pokesplash.net>
2015-02-21 19:25:28 -0500

    Setvar/copyvar/message fragments

--- pokemon/field/script.c
+++ pokemon/field/script.c
@@ -18,7 +18,10 @@ struct script_state {
     // 0x60
     int command_count;
     // 0x64
-    int vals[0x1C]; // unknown length and purpose
+    int vals[0x14]; // unknown length and purpose
+    // 0x78
+    int u78;
+    int u7c; // pad
     // 0x80
     int u5;
 };
@@ -244,5 +247,64 @@ int cmd_0031(script_state *r0){
     int r1 = read16(r0);
     int r0 = func_39550(r4, r1);
     return 0;
+}
+
+int cmd_0040(script_state *r0){
+    // 0x39fb9 - Setvar($1, $2)
+    script_state* r4 = r0;
+    int r1 = read16(r0);
+    int r0 = r0->u5;
+    int r5 = func_394b8(r0, r1);
+    script_state* r0 = r4;
+    int r0 = read16(r0);
+    ((uint16_t *)r5)[0] = (uint16_t)r0;
+    return 0;
+}
+
+int cmd_0041(script_state *r0){
+    // 0x39fdd - Copyvar($1, $2)
+    script_state* r5 = r0;
+    int r1 = read16(r0);
+    int r0 = r0->u5;
+    int r4 = func_394b8(r0, r1);
+    script_state* r0 = r5;
+    int r1 = read16(r0);
+    int r0 = r0->u5;
+    int r0 = func_394b8(r0, r1);
+    ((uint16_t *)r4)[0] = ((uint16_t *)r0)[0];
+    return 0;
+}
+
+int cmd_0042(script_state *r0){
+    // 0x3a00d - 002A($1, $2)
+    script_state* r4 = r0;
+    int r1 = read16(r0);
+    int r0 = r4->u5;
+    r0 = func_394b8(r0, r1);
+    int r5 = func_39550(r4, r1);
+    script_state* r0 = r4;
+    r1 = read16(r0);
+    int r0 = r0->u5;
+    r0 = func_394f0(r0, r1);
+    ((uint16_t *)r5)[0] = (uint16_t)r0;
+    return 0;
+}
+
+int cmd_0043(script_state *r0){
+    // 0x3a039 - Message2($1)
+    int r2 = *((unsigned char*)(r0->buf_ptr++));
+    int r1 = r0->u78;
+    func_1e2c24(r0, r1);
+    return 0;
+}
+
+int cmd_0044(script_state *r0){
+    // 0x3a2c4 - Message($1)
+    int r3 = 1;
+    int r2 = *((unsigned char*)(r0->buf_ptr++));
+    int r1 = r0->u78;
+    func_1e2bd0(r0, r1);
+    func_38b5c(r0, 0x203a2f1);
+    return 1;
 }
 
