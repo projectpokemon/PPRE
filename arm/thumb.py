58ba2722b956321c0c78cf6130546698595ee040
Alpha <alpha@pokesplash.net>
2015-02-25 22:20:59 -0500

    thumb: Add r/imm

--- arm/thumb.py
+++ arm/thumb.py
@@ -18,8 +18,8 @@ class Thumb(Disassembler):
         if high:
             data += 8
         if data == 14:
-            return 'lr'
-        return 'r{0}'.format(data)
+            return 'engine.lr'
+        return 'engine.r{0}'.format(data)
 
     @staticmethod
     def sign(value, bits):
@@ -30,7 +30,30 @@ class Thumb(Disassembler):
 
     def parse_next(self):
         cmd = self.read_value(2)
-        if cmd & 0b1111011000000000 == 0b1011010000000000:
+        if cmd & 0b1111100000000000 == 0b0001100000000000:
+            # add/sub
+            src = self.get_reg((cmd >> 3) & 0x7)
+            dest = self.get_reg(cmd & 0x7)
+            if cmd & 0x200:
+                oper = '-'
+            else:
+                oper = '+'
+            if cmd & 0x400:
+                val = (cmd >> 6) & 7
+            else:
+                val = self.get_reg((cmd >> 6) & 0x7)
+            return [self.assign(dest, self.statement(oper, src, val))]
+            # TODO: cspr
+        elif cmd & 0b1110000000000000 == 0b0010000000000000:
+            oper = (cmd >> 11) & 3
+            if oper == 2:
+                oper = '+'
+            elif oper == 3:
+                oper = '-'
+            src = self.get_reg((cmd >> 8) & 0x7)
+            val = cmd & 0xFF
+            return [self.assign(src, self.statement(oper, src, val))]
+        elif cmd & 0b1111011000000000 == 0b1011010000000000:
             regs = self.get_regs(cmd & 0x7F)
             if cmd & 0x800:
                 func = 'pop'
