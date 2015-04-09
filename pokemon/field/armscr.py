0ea985ba27d277b02f0ef89dd500b42b5af0bb44
Alpha <alpha@pokesplash.net>
2015-04-09 15:34:28 -0400

    Read commands from overlays

--- pokemon/field/armscr.py
+++ pokemon/field/armscr.py
@@ -6,11 +6,13 @@ Example usage:
 """
 
 import json
+import os
 import struct
 
 from pokemon.game import Game
 from pokemon.field.script import Script
 from util.io import BinaryIO
+from ntr.overlay import OverlayTable
 from arm.thumb import Thumb
 
 
@@ -55,29 +57,59 @@ if __name__ == '__main__':
             as header:
         header.seek(0x24)
         entry, ram_offset, size = struct.unpack('III', header.read(12))
+        header.seek(0x54)
+        overlay_count = struct.unpack('I', header.read(4))[0] >> 5
+
+    with open(os.path.join(target_game.files.directory, 'overarm9.dec.bin'))\
+            as overarm:
+        ovt = OverlayTable(overlay_count, reader=overarm)
 
     with open(os.path.join(target_game.files.directory, 'arm9.dec.bin'))\
             as handle:
+        arm_size = os.fstat(handle.fileno()).st_size
         cmd_info = command_info[target_game.game_name]
         handle.seek(cmd_info['table_ofs'])
         offsets = struct.unpack('I'*cmd_info['count'],
                                 handle.read(cmd_info['count']*4))
 
-        decompiler = Thumb(handle)
-        decompiler.functions = cmd_info['functions']
-
         skip = 0
         commands = {}
         for i, offset in enumerate(offsets):
             print(i, hex(offset))
-            decompiler.start = (offset & 0xFFFFFFFE) - ram_offset - 4
-            decompiler.reset()
-            if decompiler.read_value(4) is None:
-                skip += 1
-                continue
-            decompiler.parse()
+            offset &= 0xFFFFFFFE
             args = []
             conditional = False
+            if offset < ram_offset + arm_size:
+                decompiler = Thumb(handle)
+                decompiler.functions = cmd_info['functions']
+                decompiler.start = offset - ram_offset
+                decompiler.reset()
+                decompiler.parse()
+            else:
+                decompiler = None
+                for overlay in ovt.overlays:
+                    if overlay.address < offset < overlay.address+overlay.size:
+                        with open(os.path.join(
+                            target_game.files.directory, 'overlays_dez',
+                            'overlay_{0:04}.bin'.format(overlay.file_id)))\
+                                as overlay_handle:
+                            overlay_handle.seek(offset - overlay.address)
+                            top = struct.unpack('H', overlay_handle.read(2))[0]
+                            if top & 0xFF00 != 0xB500:
+                                # Require push {pc}
+                                continue
+                            if decompiler is not None:
+                                conditional = True
+                            decompiler = Thumb(overlay_handle)
+                            for ofs, name in cmd_info['functions'].items():
+                                ofs -= overlay.address-ram_offset
+                                decompiler.functions[ofs] = name
+                            decompiler.start = offset - overlay.address
+                            decompiler.reset()
+                            decompiler.parse()
+                if decompiler is None:
+                    skip += 1
+                    continue
             for expr in decompiler:
                 line = str(expr)
                 if 'read16' in line:
