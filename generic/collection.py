ca9edf075b8e4dfe12ecdb60799a9f4a66634499
Alpha <alpha@pokesplash.net>
2015-04-20 14:52:35 -0400

    2d Collection class

---
+++ generic/collection.py
@@ -0,0 +1,25 @@
+
+from __future__ import absolute_import
+
+from editable import Editable
+
+
+class Collection2d(Editable):
+    def define(self, entry, width, height):
+        self.width = width
+        self.height = height
+        self.array('entries', entry.base_struct, length=width*height)
+
+    def __getitem__(self, key):
+        try:
+            idx = key[1]*self.width+key[0]
+        except (IndexError, TypeError):
+            raise KeyError('2d Collection expects a two-tuple index')
+        return self.entries[idx]
+
+    def __setitem__(self, key, value):
+        try:
+            idx = key[1]*self.width+key[0]
+        except (IndexError, TypeError):
+            raise KeyError('2d Collection expects a two-tuple index')
+        self.entries[idx] = value
