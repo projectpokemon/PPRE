a03a6bd2a498e9f69ee80dd2f7b1488b1f412cbe
Alpha <alpha@pokesplash.net>
2015-04-24 21:59:26 -0400

    Skeletal Model definition

--- ntr/g3d/bmd.py
+++ ntr/g3d/bmd.py
@@ -5,6 +5,69 @@ from ntr.g3d.resdict import G3DResDict
 from util import BinaryIO
 
 
+class Model(Editable):
+    def define(self):
+        self.uint32('size_')
+        self.uint32('sbc_offset')
+        self.uint32('mat_offset')
+        self.uint32('shp_offset')
+        self.uint32('matrix_offset')
+        self.uint8('sbc_type')
+        self.uint8('scale')
+        self.uint8('tex_mode')
+        self.uint8('num_nodes')
+        self.uint8('num_materials')
+        self.uint8('num_shapes')
+        self.uint8('mtx_start')
+        self.uint8('pad17', default=0)
+        self.uint32('pos_scale_fx32')
+        self.uint32('inv_pos_scale_fx32')
+        self.uint16('num_vertex')
+        self.uint16('num_polygon')
+        self.uint16('num_triangle')
+        self.uint16('num_quad')
+        self.uint16('box_x_fx16')
+        self.uint16('box_y_fx16')
+        self.uint16('box_z_fx16')
+        self.uint16('box_w_fx16')
+        self.uint16('box_h_fx16')
+        self.uint16('box_d_fx16')
+        self.uint32('box_scale_fx32')
+        self.uint32('inv_box_scale_fx32')
+        self.nodes = []
+        self.sbc = []
+        self.materials = []
+        self.shapes = []
+        self.matrixes = []
+
+    def load(self, reader):
+        start = reader.tell()
+        Editable.load(self, reader)
+
+        self.nodes = []
+        for i in range(self.num_nodes):
+            self.nodes.append(None)  # Node
+
+        self.sbc = []
+        reader.seek(start+self.sbc_offset)
+        for i in range(self.mat_offset-self.sbc_offset):
+            self.sbc.append(None)  # SBC
+
+        self.materials = []
+        reader.seek(start+self.mat_offset)
+        for i in range(self.num_materials):
+            self.materials.append(None)  # Material
+
+        self.shapes = []
+        reader.seek(start+self.shp_offset)
+        for i in range(self.num_shapes):
+            self.shapes.append(None)  # Shape
+
+    def save(self, writer):
+        start = writer.tell()
+        writer = Editable.save(self, writer)
+
+
 class MDL(Editable):
     def define(self):
         self.string('magic', length=4, default='MDL0')
@@ -18,8 +81,7 @@ class MDL(Editable):
         self.mdldict.load(reader)
         self.models = []
         for idx in range(self.mdldict.num):
-            # self.models.append(Model(reader=reader))
-            pass
+            self.models.append(Model(reader=reader))
 
     def save(self, writer=None):
         writer = BinaryIO.writer(writer)
@@ -78,7 +140,7 @@ class BMD(Editable):
         start = writer.tell()
         writer = Editable.save(self, writer)
         offset_offset = writer.tell()
-        for i in self.numblocks:
+        for i in range(self.numblocks):
             writer.writeUInt32(0)
         block_ofs = writer.tell()-start
         with writer.seek(offset_offset):
