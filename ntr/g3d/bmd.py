881d5bae6f0cb1f75e656072feeef6ddff99f7dd
Alpha <alpha@pokesplash.net>
2015-04-24 20:10:46 -0400

    Added MDL basic definition

--- ntr/g3d/bmd.py
+++ ntr/g3d/bmd.py
@@ -1,7 +1,35 @@
 
 from generic import Editable
 from ntr.g3d.btx import TEX
+from ntr.g3d.resdict import G3DResDict
 from util import BinaryIO
+
+
+class MDL(Editable):
+    def define(self):
+        self.string('magic', length=4, default='MDL0')
+        self.uint32('size_')
+        self.mdldict = G3DResDict()
+        self.mdldict.sizeunit = 4
+        self.models = []
+
+    def load(self, reader):
+        Editable.load(self, reader)
+        self.mdldict.load(reader)
+        self.models = []
+        for idx in range(self.mdldict.num):
+            # self.models.append(Model(reader=reader))
+            pass
+
+    def save(self, writer=None):
+        writer = BinaryIO.writer(writer)
+        start = writer.tell()
+        writer = Editable.save(self, writer)
+        writer = self.mdldict.save(writer)
+        size = writer.tell()-start
+        with writer.seek(start+self.get_offset('size_')):
+            writer.writeUInt32(size)
+        return writer
 
 
 class BMD(Editable):
@@ -21,8 +49,8 @@ class BMD(Editable):
         self.uint32('size_')
         self.uint16('headersize', default=0x10)
         self.uint16('numblocks', default=2)
-        self.mdl = MDL(self)
-        self.tex = TEX(self)
+        self.mdl = MDL()
+        self.tex = TEX()
 
     def load(self, reader):
         reader = BinaryIO.reader(reader)
