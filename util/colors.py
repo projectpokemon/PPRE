3e129eeadb4ade9b5c5e9ea8bda44709e942a47d
Alpha <alpha@pokesplash.net>
2015-04-26 19:38:35 -0400

    colors.rotate_color utility

--- util/colors.py
+++ util/colors.py
@@ -13,3 +13,26 @@ def color_gen():
                 yield (int(color[0]*255), int(color[1]*255),
                        int(color[2]*255), 255)
         chunks <<= 1
+
+
+def rotate_color(color, degrees=180):
+    """Rotates the hue of an RGB color
+
+    Parameters
+    ----------
+    color : RGB[A] tuple
+        Color to transform
+    degrees : int
+        Amount to change color by. Out of 360.
+
+    Returns
+    -------
+    color : RGB[A] tuple
+        Rotated color
+    """
+    alpha = color[3:]
+    hsv = colorsys.rgb_to_hsv(*(c/255.0 for c in color[:3]))
+    hue = (hsv[0]*360+degrees) % 360
+    color = colorsys.hsv_to_rgb(hue/360.0, hsv[1], hsv[2])
+    return (int(color[0]*255), int(color[1]*255), int(color[2]*255))\
+        + tuple(alpha)
