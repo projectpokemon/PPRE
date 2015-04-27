6b59f53b05560821d1d88be06f81c59efb3f036a
Alpha <alpha@pokesplash.net>
2015-04-26 22:41:03 -0400

    Cleaned up Evolutions

--- pokemon/poketool/evo.py
+++ pokemon/poketool/evo.py
@@ -1,59 +1,17 @@
 
-from generic.editable import Editable
+from generic import Editable
+from generic.collection import SizedCollection
 from util import BinaryIO
 
 
 class Evolution(Editable):
-    def __init__(self, method, parameter, target):
-        self.method = method
-        self.restrictUInt16('method')
-        self.parameter = parameter
-        self.restrictUInt16('parameter')
-        self.target = target
-        self.restrictUInt16('target')
-
-
-class Evolutions(Editable):
-    def __init__(self, reader=None):
-        self.evos = []
-        self.restrict('evos')
-        if reader is not None:
-            self.load(reader)
-
-    def load(self, reader):
-        reader = BinaryIO.reader(reader)
-        self.evos = []
-        for i in xrange(7):
-            evo = Evolution(reader.readUInt16(), reader.readUInt16(),
-                            reader.readUInt16())
-            if evo.method or evo.target:
-                self.evos.append(evo)
-
-    def save(self, writer=None):
-        writer = writer if writer is not None else BinaryIO()
-        start = writer.tell()
-        for evo in self.evos:
-            writer.writeUInt16(evo.method)
-            writer.writeUInt16(evo.param)
-            writer.writeUInt16(evo.target)
-        writer.writePadding(start+44)
-        return writer
-
-
-from generic.editable import XEditable as Editable
-
-
-class Evolution(Editable):
-    def __init__(self):
-        Editable.__init__(self)
+    def define(self):
         self.uint16('method')
         self.uint16('param')
         self.uint16('target')
-        self.freeze()
 
 
-class Evolutions(Editable):
-    def __init__(self, reader=None):
-        Editable.__init__(self)
-        self.array('evos', Evolution().base_struct, length=7)
-        self.freeze()
+class Evolutions(SizedCollection):
+    def define(self):
+        SizedCollection.define(self, Evolution().base_struct, length=7,
+                               resizable=False)
