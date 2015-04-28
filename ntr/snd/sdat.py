d28dbe9d3b35067ce70a85bfa32ae0bc7e3e22d0
Alpha <alpha@pokesplash.net>
2015-04-28 01:30:25 -0400

    SYMB records

--- ntr/snd/sdat.py
+++ ntr/snd/sdat.py
@@ -5,8 +5,65 @@ from generic.archive import Archive
 from util import BinaryIO
 
 
+class SYMB(Editable):
+    """SDAT Symbols
+
+    These contain the "filenames" of the archive data.
+    """
+    record_names = ['SEQ', 'SEQARC', 'BANK', 'WAVEARC', 'PLAYER',
+                    'GROUP', 'PLAYER2', 'STRM']
+
+    def define(self, sdat):
+        self.sdat = sdat
+        self.string('magic', length=4, default='SYMB')
+        self.uint32('size_')
+        self.array('record_offsets', self.uint32, length=14)
+        self.records = {}
+
+    def load(self, reader):
+        reader = BinaryIO.reader(reader)
+        start = reader.tell()
+        Editable.load(self, reader)
+        self.records = {}
+        for record_ofs, record_name in zip(self.record_offsets,
+                                           self.record_names):
+            if not record_ofs:
+                continue
+            reader.seek(start+record_ofs)
+            num = reader.readUInt32()
+            if record_name == 'SEQARC':
+                offsets = []
+                for i in range(num):
+                    offsets.append(reader.readUInt32(), reader.readUInt32())
+                entries = []
+                for i, (offset, sub_offset) in enumerate(offsets):
+                    reader.seek(start+offset)
+                    name = reader.readString()
+                    prefix = (record_name, name)
+                    entries += self.load_entries(reader, start, prefix, num)
+            else:
+                entries = self.load_entries(reader, start, (record_name,), num)
+            self.records[record_name] = entries
+
+    @staticmethod
+    def load_entries(reader, base_offset, prefix, num):
+        offsets = []
+        for i in range(num):
+            offsets.append(reader.readUInt32())
+        entries = []
+        for i, offset in enumerate(offsets):
+            if not offset:
+                entries.append(prefix+(str(i),))
+                continue
+            reader.seek(base_offset+offset)
+            entries.append(prefix+(reader.readString(),))
+        return entries
+
+
 class SDAT(Archive, Editable):
     """Sound Data Archive"""
+    extension = ''  # filenames include their own extension
+
     def define(self):
         self.string('magic', length=4, default='SDAT')
         self.uint16('endian', default=0xFEFF)
@@ -20,9 +77,9 @@ class SDAT(Archive, Editable):
         block_ofs.freeze()
         self.array('block_offsets', block_ofs.base_struct, length=8)
         self.symb = SYMB(self)
-        self.fat = FAT(self)
-        self.info = INFO(self)
-        self.file = FILE(self)
+        self.fat = None  # FAT(self)
+        self.info = None  # INFO(self)
+        self.file = None  # FILE(self)
 
     @property
     def files(self):
@@ -35,6 +92,8 @@ class SDAT(Archive, Editable):
         assert self.magic == 'SDAT'
         for block_ofs, block in zip(self.block_offsets, [
                 self.symb, self.fat, self.info, self.file]):
+            if not block_ofs.block_offset:
+                continue
             reader.seek(start+block_ofs.block_offset)
             block.load(reader)
 
