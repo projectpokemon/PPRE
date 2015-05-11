f5c79bcada26043d2cc0f377c94c77f28452f838
Alpha <alpha@pokesplash.net>
2015-05-10 23:43:33 -0400

    World.reshape()

--- pokemon/field/map_matrix.py
+++ pokemon/field/map_matrix.py
@@ -13,6 +13,53 @@ class MapMatrix(Editable):
         self.uint8('namelen')
         self.name = ''  # pascal string
 
+    def reshape(self, width, height, copy=True):
+        """Reshape the world to a new width/height
+
+        Parameters
+        ----------
+        width : int
+            New width
+        height : int
+            New height
+        copy : Bool
+            If true, copy the existing data in
+        """
+        self.width = width
+        self.height = height
+        if self.has_map_definition_ids:
+            old_collection = self.map_definitions
+            self.map_definitions = Collection2d(self.uint16, self.width,
+                                                self.height)
+            if copy:
+                for x in range(self.width):
+                    for y in range(self.height):
+                        try:
+                            self.map_definitions[x, y] = old_collection[x, y]
+                        except:
+                            break
+        if self.has_mystery_zone:
+            old_collection = self.mystery_details
+            self.mystery_details = Collection2d(self.uint8, self.width,
+                                                self.height)
+            if copy:
+                for x in range(self.width):
+                    for y in range(self.height):
+                        try:
+                            self.mystery_details[x, y] = old_collection[x, y]
+                        except:
+                            break
+        old_collection = self.land_data_maps
+        self.land_data_maps = Collection2d(self.uint16, self.width,
+                                           self.height)
+        if copy:
+            for x in range(self.width):
+                for y in range(self.height):
+                    try:
+                        self.land_data_maps[x, y] = old_collection[x, y]
+                    except:
+                        break
+
     def load(self, reader):
         reader = BinaryIO.reader(reader)
         Editable.load(self, reader)
