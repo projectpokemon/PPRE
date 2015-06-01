ade13798bd31c858e8a8014a7fc3d09ef08bf0d9
Alpha <alpha@pokesplash.net>
2015-06-01 06:39:48 -0400

    cx_Freeze PPRE_DIR const

--- ppre/__init__.py
+++ ppre/__init__.py
@@ -5,9 +5,19 @@ import sys
 __all__ = ['PPRE_DIR', 'open_resource']
 
 try:
-    PPRE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-except NameError:
-    PPRE_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+    if sys.frozen:
+        # cx_freeze executable
+        PPRE_DIR = os.path.dirname(sys.executable)
+    else:
+        raise AttributeError
+except AttributeError:
+    try:
+        # native source
+        PPRE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..'))
+    except NameError:
+        # reliable fallback
+        PPRE_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 
 def open_resource(*path, **kwargs):
